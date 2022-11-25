1108e270245a2ad32d8dc3d2ed35b4036bf9bb93
Christine TAN <christine.tan@efrei.net>
2022-11-25 21:07:46 +0100

    add data assignmt 2

--- Assignmt2/problem2.py
+++ Assignmt2/problem2.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import tensorflow as tf
+import numpy as np
 import os
 
 path_file = os.getcwd()
@@ -13,3 +14,26 @@ tflite_model = converter.convert()
 # Save the model.
 with open('model.tflite', 'wb') as f:
   f.write(tflite_model)
+
+
+# Load the TFLite model and allocate tensors.
+interpreter = tf.lite.Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
+
+# Get input and output tensors.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+# Test the model on random input data.
+input_shape = input_details[0]['shape']
+input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+interpreter.set_tensor(input_details[0]['index'], input_data)
+
+interpreter.invoke()
+
+# The function `get_tensor()` returns a copy of the tensor data.
+# Use `tensor()` in order to get a pointer to the tensor.
+output_data = interpreter.get_tensor(output_details[0]['index'])
+print(output_data)
+
+
