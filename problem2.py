d94111475f627fbb5f8bba71082e706628175c86
Christine TAN <christine.tan@efrei.net>
2022-12-03 23:37:05 +0100

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -7,11 +7,13 @@ from tensorflow import lite
 from problem1 import load_img
 import time
 
+import splitfolders
+
 path_file = os.getcwd()
 
 model_dir = os.path.join(path_file, 'model.h5')
 output_dir = os.path.join(path_file, 'train_val_test')
-
+img_dir = os.path.join(path_file, 'data_img')
 
 # Load model
 model = load_model(model_dir)
@@ -30,7 +32,7 @@ end = time.time()
 
 print("TIMER : ", int(end - start), "s\n")
 
-# TIMER :  8 s
+# TIMER :  9 s
 
 # Save the model TFLite
 with open('model.tflite', 'wb') as f:
@@ -48,18 +50,23 @@ interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-
-# Load val dataset
-val_dir = os.path.join(path_file, 'train_val_test', 'val')
-list_val_img = glob.glob(val_dir + "/*/*.png")
-val_img, val_labels = load_img(list_val_img)
+# Create folder for test validation
+if not os.path.exists(output_dir):
+    os.mkdir(output_dir)
+    # To only split into training and validation set, set a tuple to `ratio`, i.e, `(.8, .2)`.
+    splitfolders.ratio(img_dir, output=output_dir, seed=1337, ratio=(.8, .1, .1))
+    
+# Load test dataset
+test_dir = os.path.join(path_file, 'train_val_test', 'test')
+list_test_img = glob.glob(test_dir + "/*/*.png")
+test_img, test_labels = load_img(list_test_img)
 
 # Test with 10 img
 nb_test_img = 10
 
 for i in range(nb_test_img):
-    img_8 = val_img[i]
-    label = val_labels[i]
+    img_8 = test_img[i]
+    label = test_labels[i]
     img_32 = img_8.astype('float32')
     img_32 = np.expand_dims(img_32, axis=0)
     interpreter.set_tensor(input_details[0]['index'], img_32)
@@ -75,63 +82,63 @@ for i in range(nb_test_img):
     print("_______________________\n")
 
 """
-1/1 [==============================] - 0s 196ms/step
- OUTPUT TFLITE  2 0.54476225
- OUTPUT MODEL   2 0.54476285
+1/1 [==============================] - 0s 281ms/step
+ OUTPUT TFLITE  2 0.5272661
+ OUTPUT MODEL   2 0.5272658
  LABEL   2
 _______________________
 
-1/1 [==============================] - 0s 47ms/step
- OUTPUT TFLITE  3 0.9597202
- OUTPUT MODEL   3 0.95972043
+1/1 [==============================] - 0s 45ms/step
+ OUTPUT TFLITE  3 0.9516666
+ OUTPUT MODEL   3 0.9516667
  LABEL   3
 _______________________
 
-1/1 [==============================] - 0s 48ms/step
- OUTPUT TFLITE  5 0.4582212
- OUTPUT MODEL   5 0.45822024
+1/1 [==============================] - 0s 45ms/step
+ OUTPUT TFLITE  3 0.5781447
+ OUTPUT MODEL   3 0.5781444
  LABEL   3
 _______________________
 
-1/1 [==============================] - 0s 51ms/step
- OUTPUT TFLITE  1 0.9450509
- OUTPUT MODEL   1 0.9450509
+1/1 [==============================] - 0s 44ms/step
+ OUTPUT TFLITE  1 0.96721864
+ OUTPUT MODEL   1 0.96721864
  LABEL   1
 _______________________
 
-1/1 [==============================] - 0s 47ms/step
- OUTPUT TFLITE  2 0.8629077
- OUTPUT MODEL   2 0.86290735
- LABEL   2
-_______________________
-
-1/1 [==============================] - 0s 47ms/step
- OUTPUT TFLITE  2 0.65619695
- OUTPUT MODEL   2 0.6561966
+1/1 [==============================] - 0s 45ms/step
+ OUTPUT TFLITE  2 0.9204558
+ OUTPUT MODEL   2 0.9204553
  LABEL   2
 _______________________
 
 1/1 [==============================] - 0s 46ms/step
- OUTPUT TFLITE  0 0.49432638
- OUTPUT MODEL   0 0.49432668
+ OUTPUT TFLITE  2 0.6046625
+ OUTPUT MODEL   2 0.60466224
+ LABEL   2
+_______________________
+
+1/1 [==============================] - 0s 59ms/step
+ OUTPUT TFLITE  6 0.46202195
+ OUTPUT MODEL   6 0.46202204
  LABEL   9
 _______________________
 
-1/1 [==============================] - 0s 62ms/step
- OUTPUT TFLITE  6 0.43688843
- OUTPUT MODEL   6 0.4368882
+1/1 [==============================] - 0s 54ms/step
+ OUTPUT TFLITE  4 0.7411054
+ OUTPUT MODEL   4 0.7411053
  LABEL   4
 _______________________
 
 1/1 [==============================] - 0s 51ms/step
- OUTPUT TFLITE  1 0.9473194
- OUTPUT MODEL   1 0.9473196
+ OUTPUT TFLITE  1 0.8826441
+ OUTPUT MODEL   1 0.88264394
  LABEL   1
 _______________________
 
-1/1 [==============================] - 0s 48ms/step
- OUTPUT TFLITE  1 0.8291788
- OUTPUT MODEL   1 0.82917905
+1/1 [==============================] - 0s 64ms/step
+ OUTPUT TFLITE  1 0.997813
+ OUTPUT MODEL   1 0.997813
  LABEL   1
 _______________________
 
