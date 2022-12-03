901f72e74895e357f57da78c909df7bd11f509ab
Christine TAN <christine.tan@efrei.net>
2022-12-03 23:29:48 +0100

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -5,18 +5,32 @@ import glob
 from tensorflow.keras.models import load_model
 from tensorflow import lite
 from problem1 import load_img
+import time
 
 path_file = os.getcwd()
 
 model_dir = os.path.join(path_file, 'model.h5')
 output_dir = os.path.join(path_file, 'train_val_test')
 
+
 # Load model
 model = load_model(model_dir)
+
+# Start time
+
+start = time.time()
 
 # Convert the model
 converter = lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
+
+# End
+
+end = time.time()
+
+print("TIMER : ", int(end - start), "s\n")
+
+# TIMER :  8 s
 
 # Save the model TFLite
 with open('model.tflite', 'wb') as f:
@@ -34,79 +48,98 @@ interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-# Test the model on random input data.
-input_shape = input_details[0]['shape']
-input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
-interpreter.set_tensor(input_details[0]['index'], input_data)
 
-interpreter.invoke()
+# Load val dataset
+val_dir = os.path.join(path_file, 'train_val_test', 'val')
+list_val_img = glob.glob(val_dir + "/*/*.png")
+val_img, val_labels = load_img(list_val_img)
 
-# The function `get_tensor()` returns a copy of the tensor data.
-# Use `tensor()` in order to get a pointer to the tensor.
-output_data = interpreter.get_tensor(output_details[0]['index'])
+# Test with 10 img
+nb_test_img = 10
 
-for i in range(11):
-    print(" Result TFLite :: class ", i, " : ", output_data[0][i])
+for i in range(nb_test_img):
+    img_8 = val_img[i]
+    label = val_labels[i]
+    img_32 = img_8.astype('float32')
+    img_32 = np.expand_dims(img_32, axis=0)
+    interpreter.set_tensor(input_details[0]['index'], img_32)
 
+    interpreter.invoke()
+    
+    output_tflite = interpreter.get_tensor(output_details[0]['index'])
+    output_model = model.predict(img_32)
 
-# Load test dataset
-test_dir = os.path.join(path_file, 'train_val_test', 'test')
-list_train_img = glob.glob(test_dir + "/*/*.png")
-test_img, test_labels = load_img(list_train_img)
-print(test_img.shape)
-print(test_labels.shape)
-pred = model.predict(test_img)
+    print(" OUTPUT TFLITE ", output_tflite[0].argmax(), max(output_tflite[0]))
+    print(" OUTPUT MODEL  ", output_model[0].argmax(), max(output_model[0]))
+    print(" LABEL  ", label)
+    print("_______________________\n")
 
-for i in range(11):
-    print(" Result MODEL  :: class ", i, " : ", pred[0][i])
-    print(" Result TFLite :: class ", i, " : ", output_data[0][i])
-    print("_______________________")
-    print(" ")
+"""
+1/1 [==============================] - 0s 196ms/step
+ OUTPUT TFLITE  2 0.54476225
+ OUTPUT MODEL   2 0.54476285
+ LABEL   2
+_______________________
+
+1/1 [==============================] - 0s 47ms/step
+ OUTPUT TFLITE  3 0.9597202
+ OUTPUT MODEL   3 0.95972043
+ LABEL   3
+_______________________
+
+1/1 [==============================] - 0s 48ms/step
+ OUTPUT TFLITE  5 0.4582212
+ OUTPUT MODEL   5 0.45822024
+ LABEL   3
+_______________________
+
+1/1 [==============================] - 0s 51ms/step
+ OUTPUT TFLITE  1 0.9450509
+ OUTPUT MODEL   1 0.9450509
+ LABEL   1
+_______________________
+
+1/1 [==============================] - 0s 47ms/step
+ OUTPUT TFLITE  2 0.8629077
+ OUTPUT MODEL   2 0.86290735
+ LABEL   2
+_______________________
+
+1/1 [==============================] - 0s 47ms/step
+ OUTPUT TFLITE  2 0.65619695
+ OUTPUT MODEL   2 0.6561966
+ LABEL   2
+_______________________
+
+1/1 [==============================] - 0s 46ms/step
+ OUTPUT TFLITE  0 0.49432638
+ OUTPUT MODEL   0 0.49432668
+ LABEL   9
+_______________________
+
+1/1 [==============================] - 0s 62ms/step
+ OUTPUT TFLITE  6 0.43688843
+ OUTPUT MODEL   6 0.4368882
+ LABEL   4
+_______________________
+
+1/1 [==============================] - 0s 51ms/step
+ OUTPUT TFLITE  1 0.9473194
+ OUTPUT MODEL   1 0.9473196
+ LABEL   1
+_______________________
+
+1/1 [==============================] - 0s 48ms/step
+ OUTPUT TFLITE  1 0.8291788
+ OUTPUT MODEL   1 0.82917905
+ LABEL   1
+_______________________
 
 
 """
- Result MODEL  :: class  0  :  0.09775572
- Result TFLite :: class  0  :  0.09203777
-_______________________
- 
- Result MODEL  :: class  1  :  0.25075567
- Result TFLite :: class  1  :  0.009348555
-_______________________
- 
- Result MODEL  :: class  2  :  0.52726597
- Result TFLite :: class  2  :  0.0032387807
-_______________________
- 
- Result MODEL  :: class  3  :  4.4501896e-05
- Result TFLite :: class  3  :  0.00089329126
-_______________________
- 
- Result MODEL  :: class  4  :  0.012733475
- Result TFLite :: class  4  :  0.0121793905
-_______________________
- 
- Result MODEL  :: class  5  :  0.0036333448
- Result TFLite :: class  5  :  0.0076124906
-_______________________
- 
- Result MODEL  :: class  6  :  0.107321255
- Result TFLite :: class  6  :  0.70355636
-_______________________
- 
- Result MODEL  :: class  7  :  1.93319e-06
- Result TFLite :: class  7  :  0.00044466805
-_______________________
- 
- Result MODEL  :: class  8  :  3.5825316e-05
- Result TFLite :: class  8  :  0.14504051
-_______________________
- 
- Result MODEL  :: class  9  :  0.00045007924
- Result TFLite :: class  9  :  0.00561996
-_______________________
- 
- Result MODEL  :: class  10  :  2.2392553e-06
- Result TFLite :: class  10  :  0.020028291
-_______________________
- 
-"""
+print("END COMPARAISON\n")
+
+print("SIZE OF TFLite : 59 591 Ko")
+print("SIZE OF MODEL  : 178 860 Ko")
+
+print("TFLite represents 33% of MODEL's size")
